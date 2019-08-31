18d46addb00c118a54da6e4b2b0f561285253484
Gareth van der Berg <gareth.shaun@gmail.com>
2019-08-31 11:56:57 +0200

    comment out everything except for resource group

--- src/output.tf
+++ src/output.tf
@@ -6,18 +6,18 @@ output "resource_group_location" {
   value = azurerm_resource_group.this.location
 }
 
-output "container_registry_id" {
-  value = azurerm_container_registry.this.id
-}
+# output "container_registry_id" {
+#   value = azurerm_container_registry.this.id
+# }
 
-output "container_registry_login_server" {
-  value = azurerm_container_registry.this.login_server
-}
+# output "container_registry_login_server" {
+#   value = azurerm_container_registry.this.login_server
+# }
 
-output "container_registry_admin_username" {
-  value = azurerm_container_registry.this.admin_username
-}
+# output "container_registry_admin_username" {
+#   value = azurerm_container_registry.this.admin_username
+# }
 
-output "container_registry_admin_password" {
-  value = azurerm_container_registry.this.admin_password
-}
+# output "container_registry_admin_password" {
+#   value = azurerm_container_registry.this.admin_password
+# }
