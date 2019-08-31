1fc9dfd3bdb4e8ef2038bfe20d6c7942b60b3c1f
Gareth van der Berg <gareth.shaun@gmail.com>
2019-08-31 11:12:05 +0200

    set admin_enabled to true

--- src/deploy.tf
+++ src/deploy.tf
@@ -8,7 +8,7 @@ resource "azurerm_container_registry" "this" {
   name                = var.container_registry_name
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
-  admin_enabled       = false
+  admin_enabled       = true
   sku                 = var.sku_name
   tags                = var.tags
   depends_on          = [azurerm_resource_group.this]
